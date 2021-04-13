9190b14cd0e47ccc2cfc753293a428d33d48f036
Clarence <69087007+clarence-de-coder@users.noreply.github.com>
2021-04-13 16:37:21 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,60 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
+app.get("/api/timestamp/", (req, res) => {
+  let date = new Date()
+
+  res.json({
+    unix: date.valueOf(), 
+    utc: date.toUTCString()
+  })
+})
+
+app.get("/api/timestamp/:date?", (req, res) => {
+  let date = new Date(req.params.date)
+  // if date is NaN tries to cast it as a number and see if it creates a valid date
+  if (isNaN(date)) {
+    date = new Date(parseInt(req.params.date))
+    // if it is still NaN, throws an error
+    if (isNaN(date)) {
+      res.json({
+        error: "Invalid Date"
+      })
+    }
+  }
+
+  res.json({
+    unix: date.valueOf(),
+    utc: date.toUTCString()
+  })
+})
